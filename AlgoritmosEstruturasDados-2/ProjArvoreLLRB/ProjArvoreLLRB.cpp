acca7cb7c220ea2b8baff4230532ba5dc99ba8a3
Mesquita <mickaelmendes50@gmail.com>
2020-09-29 20:56:01 -0300

    aed2: llrb: Add random funcitons
    Signed-off-by: Mesquita <mickaelmendes50@gmail.com>

--- AlgoritmosEstruturasDados-2/ProjArvoreLLRB/ProjArvoreLLRB.cpp
+++ AlgoritmosEstruturasDados-2/ProjArvoreLLRB/ProjArvoreLLRB.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <vector>
 #include "ArvoreLLRB.h"
 
 using namespace std;
@@ -17,8 +18,9 @@ void mostra_TempoContador(clock_t tempo_inicial, clock_t tempo_final) {
 int main() {
     ArvLLRB * llrb = cria_ArvLLRB();
     clock_t tempo_inicial, tempo_final;
+    vector<int> vector;
 
-    // LLRB
+    // LLRB 10000
     // Inserção
     // Crescente
     tempo_inicial = clock();
@@ -26,6 +28,19 @@ int main() {
         insere_ArvLLRB(llrb, i);
     }
     tempo_final = clock();
+    mostra_TempoContador(tempo_inicial, tempo_final);
+
+    // Aleatório
+    tempo_inicial = clock();
+    vector.clear();
+    for (int i = 0; i < 10000; i++) {
+        int random;
+        random = rand()%10000;
+        vector.push_back(random);
+        insere_ArvLLRB(llrb, vector[i]);
+    }
+    tempo_final = clock();
+    cout << "Aleatorio" << endl;
     mostra_TempoContador(tempo_inicial, tempo_final);
 
     // Consulta
@@ -37,6 +52,15 @@ int main() {
     tempo_final = clock();
     mostra_TempoContador(tempo_inicial, tempo_final);
 
+    // Aleatório
+    tempo_inicial = clock();
+    for (int i = 0; i < 10000; i++) {
+        insere_ArvLLRB(llrb, vector[i]);
+    }
+    tempo_final = clock();
+    cout << "Aleatorio" << endl;
+    mostra_TempoContador(tempo_inicial, tempo_final);
+
     // Remoção
     // Crescente
     tempo_inicial = clock();
@@ -44,6 +68,15 @@ int main() {
         remove_ArvLLRB(llrb, i);
     }
     tempo_final = clock();
+    mostra_TempoContador(tempo_inicial, tempo_final);
+
+    // Aleatório
+    tempo_inicial = clock();
+    for (int i = 0; i < 10000; i++) {
+        remove_ArvLLRB(llrb, vector[i]);
+    }
+    tempo_final = clock();
+    cout << "Aleatorio" << endl;
     mostra_TempoContador(tempo_inicial, tempo_final);
 
     return 0;
