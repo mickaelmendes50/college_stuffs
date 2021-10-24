fdc033dffaadee831725ec20f4e4924aa7e1865b
Mesquita <mesquita@aospa.co>
2021-10-24 00:02:16 -0300

    ip: trabalho-final: Add option to register a new client
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- IntroducaoProgramacao/listas-extra/Final/trabalho-final.c
+++ IntroducaoProgramacao/listas-extra/Final/trabalho-final.c
@@ -11,24 +11,89 @@
 #define TAM_TELEFONE 15
 #define TAM_MAX_ENDERECO 50
 
+/**
+ * Tipos de dados 
+ */
+// Cria um novo tipo de dados chamado Cliente com 
+// as caracteristicas descritas
+typedef struct {
+    int codigo;
+    char nome[TAM_MAX_NOME + 1];
+    char cpf[TAM_CPF + 1];
+    char telefone[TAM_TELEFONE + 1];
+    char endereco[TAM_MAX_ENDERECO + 1];
+} Cliente;
+// Intancia os clientes de forma global
+// para estar definido em qualquer escopo
+Cliente clientes[QUANT_MAX_CLIENTES + 1];
+// Vamos definir um contador para auxiliar no
+// cadastro de novos clientes
+int contador_clientes = 0;
+
+/**
+ * Utilitários
+ */
+// Função de cadastro dos clientes
+int cadastrar_cliente() {
+    int codigo;
+    char nome[TAM_MAX_NOME + 1];
+    char cpf[TAM_CPF + 1];
+    char telefone[TAM_TELEFONE + 1];
+    char endereco[TAM_MAX_ENDERECO + 1];
+
+    printf("\n===== Informações =====\n");
+    printf("Código: ");
+    scanf("%d", &codigo);
+    for (int i = 0; i < contador_clientes; i++) {
+        if (clientes[i].codigo == codigo) {
+            printf("Cliente já cadastrado\n");
+            return 0;
+        }
+    }
+    printf("Nome: ");
+    scanf("%s", nome);
+    printf("CPF/CPNJ: ");
+    scanf("%s", cpf);
+    printf("Telefone: ");
+    scanf("%s", telefone);
+    printf("Endereço: ");
+    scanf("%s", endereco);
+
+    clientes[contador_clientes].codigo = codigo;
+    *clientes[contador_clientes].nome = *nome;
+    *clientes[contador_clientes].cpf = *cpf;
+    *clientes[contador_clientes].telefone = *telefone;
+    *clientes[contador_clientes].endereco = *endereco;
+
+    // Incrementa o número de clientes
+    contador_clientes++;
+    return 1;
+}
+
+/**
+ * Menus de interação
+ */
 // Menu de gestão de clientes
 void menu_gerenciar_cliente() {
-    printf("============ Gerenciar Clientes ============\n");
-    printf("Digite um comando para prosseguir:\n");
-    printf("C – Cadastrar um cliente\n");
-    printf("L – Listar todos os clientes cadastrados\n");
-    printf("B – Buscar cliente já cadastrado\n");
-    printf("A – Atualizar um cliente cadastrado\n");
-    printf("E – Excluir um cliente cadastrado\n");
-    printf("S – Sair\n");
-
-    char opcao = getchar();
-    getchar();
-
+    char opcao;
     do {
+        printf("============ Gerenciar Clientes ============\n");
+        printf("Digite um comando para prosseguir:\n");
+        printf("C – Cadastrar um cliente\n");
+        printf("L – Listar todos os clientes cadastrados\n");
+        printf("B – Buscar cliente já cadastrado\n");
+        printf("A – Atualizar um cliente cadastrado\n");
+        printf("E – Excluir um cliente cadastrado\n");
+        printf("S – Sair\n");
+        scanf(" %c", &opcao);
+
         switch (opcao) {
             case 'C':
-                
+                if (cadastrar_cliente()) {
+                    printf("\nCadastro concluído com sucesso\n\n");
+                    break;
+                }
+                printf("\nOps! Algo deu errado\n\n");
                 break;
             
             case 'S':
@@ -82,19 +147,6 @@ void menu_principal() {
     } while (opcao != 'S');
     printf("\nAté mais!\n");
 }
-
-// Cria um novo tipo de dados chamado Cliente com 
-// as caracteristicas descritas
-typedef struct {
-    int codigo;
-    char nome[TAM_MAX_NOME + 1];
-    char cpf[TAM_CPF + 1];
-    char telefone[TAM_TELEFONE + 1];
-    char endereco[TAM_MAX_ENDERECO + 1];
-} Cliente;
-// Intancia os clientes de forma global
-// para estar definido em qualquer escopo
-Cliente clientes[QUANT_MAX_CLIENTES + 1];
 
 int main(int argc, char const *argv[]) {
 
